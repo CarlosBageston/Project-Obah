e1bbadc6591cfc41eff67bf663455d433fca754e
Carlos Eduardo Bageston <carlos.e.bageston@outlook.com>
2023-08-12 05:52:19 -0300

    inicando tela de pedidos

--- src/Pages/Contato/index.tsx
+++ src/Pages/Contato/index.tsx
@@ -26,41 +26,69 @@ import {
     ContainerIcons,
     ContainerTitle,
     ContainerContato,
+    SubTitleWhatsapp,
 } from './style';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { Alert, AlertTitle } from '@mui/material';
 
 
 
 export default function Contato() {
     const targetRef = useRef<HTMLDivElement>(null);
+    const [submitForm, setSubmitForm] = useState<boolean | undefined>(undefined);
+    const [key, setKey] = useState<number>(0);
 
     const initialValues: ContatoModel = ({
         msgContato: '',
         nmContato: '',
         tfContato: '',
-        emailContato: ''
+        emailContato: '',
+        msgOpenContato: false
     });
 
 
-    const { values, errors, touched, handleBlur, handleSubmit, setFieldValue } = useFormik<ContatoModel>({
+    const { values, errors, touched, handleBlur, handleSubmit, setFieldValue, resetForm } = useFormik<ContatoModel>({
         validateOnBlur: true,
         validateOnChange: true,
         initialValues,
         validationSchema: Yup.object().shape({
             msgContato: Yup.string().required('Campo obrigatório'),
             nmContato: Yup.string().required('Campo obrigatório'),
-            tfContato: Yup.string().required('Campo obrigatório'),
+            tfContato: Yup.string().required('Campo obrigatório').min(11, "Digite o número de telefone completo"),
             emailContato: Yup.string().email('E-mail invalido').optional().nullable()
         }),
-        onSubmit: () => console.log('deu boa'),
+        onSubmit: hundleSubmitForm,
     });
 
+    function cleanState() {
+        setFieldValue('msgContato', '')
+        setFieldValue('nmContato', '')
+        setFieldValue('tfContato', '')
+        setFieldValue('emailContato', '')
+        setKey(Math.random());
+    }
 
+    //envia informações para o banco
+    async function hundleSubmitForm() {
+        await addDoc(collection(db, "Pedidos"), {
+            ...values
+        }).then(() => {
+            setSubmitForm(true);
+            setTimeout(() => { setSubmitForm(undefined) }, 3000)
+        }).catch(() => {
+            setSubmitForm(false);
+            setTimeout(() => { setSubmitForm(undefined) }, 3000)
+        });
+        resetForm()
+        cleanState()
+    }
     return (
         <>
             <Box>
                 {/*toolTip */}
-
                 <ToolTip tooltipRef={targetRef} />
+
                 <Imagem id='contato' />
                 <ContainerAll>
 
@@ -89,6 +117,7 @@ export default function Contato() {
                                         SorveteriaObah!
                                     </Ancora>
                                 </div>
+                                <SubTitleWhatsapp>Se preferir entre em contato pelo whatsapp</SubTitleWhatsapp>
                                 <div>
                                     <Ancora
                                         target='_blank'
@@ -103,6 +132,7 @@ export default function Contato() {
                             <TitleBox>Entre em contato</TitleBox>
                             <SubTitleBox>Preencha o formulario abaixo</SubTitleBox>
                             <Input
+                                key={`nmContato-${key}`}
                                 label='Nome'
                                 name='nmContato'
                                 onBlur={handleBlur}
@@ -110,18 +140,24 @@ export default function Contato() {
                                 onChange={e => setFieldValue(e.target.name, e.target.value)}
                                 error={touched.nmContato && errors.nmContato ? errors.nmContato : ''}
                                 styleLabel={{ color: '#FFFFFF' }}
+                                style={{ color: '#ffffff' }}
+                                raisedLabel={values.nmContato ? true : false}
                             />
                             <Input
+                                key={`tfContato-${key}`}
                                 maxLength={12}
                                 label='Telefone'
                                 name='tfContato'
                                 onBlur={handleBlur}
                                 value={formatPhone(values.tfContato)}
-                                onChange={e => setFieldValue(e.target.name, e.target.value)}
+                                onChange={e => setFieldValue(e.target.name, e.target.value.replace(/[^0-9]/g, ''))}
                                 error={touched.tfContato && errors.tfContato ? errors.tfContato : ''}
                                 styleLabel={{ color: '#FFFFFF' }}
+                                style={{ color: '#ffffff' }}
+                                raisedLabel={values.tfContato ? true : false}
                             />
                             <Input
+                                key={`emailContato-${key}`}
                                 label='Email'
                                 name='emailContato'
                                 onBlur={handleBlur}
@@ -129,8 +165,11 @@ export default function Contato() {
                                 onChange={e => setFieldValue(e.target.name, e.target.value)}
                                 error={touched.emailContato && errors.emailContato ? errors.emailContato : ''}
                                 styleLabel={{ color: '#FFFFFF' }}
+                                style={{ color: '#ffffff' }}
+                                raisedLabel={values.emailContato ? true : false}
                             />
                             <Input
+                                key={`msgContato-${key}`}
                                 label='Mensagem'
                                 name='msgContato'
                                 onBlur={handleBlur}
@@ -138,14 +177,42 @@ export default function Contato() {
                                 onChange={e => setFieldValue(e.target.name, e.target.value)}
                                 error={touched.msgContato && errors.msgContato ? errors.msgContato : ''}
                                 styleLabel={{ color: '#FFFFFF' }}
+                                style={{ color: '#ffffff' }}
+                                raisedLabel={values.msgContato ? true : false}
                             />
-
                             <Button
                                 type={'button'}
                                 children={'Enviar'}
                                 onClick={handleSubmit}
                                 style={{ margin: '15px 0px' }}
                             />
+                            {submitForm === undefined ? null :
+                                submitForm ? (
+                                    <Alert severity="success"
+                                        style={{
+                                            position: 'absolute',
+                                            width: '25rem'
+                                        }}
+                                    >
+                                        <AlertTitle>
+                                            <strong>Sucesso</strong>
+                                        </AlertTitle>
+                                        Mensagem enviada com <strong>Sucesso!</strong>
+                                    </Alert>
+                                ) : (
+                                    <Alert severity="error"
+                                        style={{
+                                            position: 'absolute',
+                                            width: '25rem'
+                                        }}
+                                    >
+                                        <AlertTitle>
+                                            <strong>Erro</strong>
+                                        </AlertTitle>
+                                        Erro ao Enviar mensagem. <strong>Tente novamente</strong>
+                                    </Alert>
+                                )
+                            }
                         </ContainerBox>
                     </ContainerContato>
                 </ContainerAll>

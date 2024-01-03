d079e0268fef350b14acb868edb9e8c9ee520292
Carlos Eduardo <carlos.bageston@kepha.com.br>
2024-01-03 12:24:02 -0300

    colocando tsDoc nos novos metodos

--- src/hooks/formatCurrency.ts
+++ src/hooks/formatCurrency.ts
@@ -1,7 +1,17 @@
 
-
+/**
+ * Hook personalizado para formatação de valores monetários em reais (BRL).
+ *
+ * @returns Objeto contendo funções para formatação e conversão de valores monetários.
+ */
 export default function useFormatCurrency(){
 
+    /**
+     * Formata um valor monetário brasileiro a partir de uma string.
+     *
+     * @param valor - A string contendo o valor a ser formatado.
+     * @returns O valor formatado como moeda brasileira (R$).
+     */
     function formatBrazilianCurrency(valor: string) {
         // Remove todos os caracteres não numéricos
         const cleanedValue = valor.replace(/[^\d.,]/g, '').replace(',', '.');
@@ -17,7 +27,12 @@ export default function useFormatCurrency(){
 
         return formattedText;
     }
-    
+    /**
+     * Formata um valor monetário brasileiro a partir de um número.
+     *
+     * @param valor - O valor a ser formatado.
+     * @returns O valor formatado como moeda brasileira (R$).
+     */
     function formatBrazilianCurrencyTable(valor: number) {
         // Formata como moeda brasileira (R$)
         const formattedText = valor.toLocaleString('pt-BR', {
@@ -27,6 +42,13 @@ export default function useFormatCurrency(){
 
         return formattedText;
     }
+
+    /**
+     * Converte uma string para um número de ponto flutuante.
+     *
+     * @param valor - A string contendo o valor a ser convertido.
+     * @returns O valor convertido para número de ponto flutuante.
+     */
     function convertToNumber(valor: string) {
         // Remove letras e substitui ponto por vírgula
         const cleanedValue = valor.replace(/[^0-9,.]/g, '').replace(/[.]/g, '');

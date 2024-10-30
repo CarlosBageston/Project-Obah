d9f952877dd9e5cb3b3e6684b9c16ce354503ad1
Carlos Eduardo <carlos.bageston@bubo.tech>
2024-10-30 12:56:34 -0300

    novo componente de notificar usuario

---
+++ src/Components/snackBar/customsnackbar.tsx
@@ -0,0 +1,67 @@
+import { Alert, IconButton, Snackbar } from "@mui/material";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import CloseIcon from '@mui/icons-material/Close';
+import { useDispatch } from "react-redux";
+import { setError } from "../../store/reducer/reducer";
+
+interface SnackBarProps {
+    message: string;
+    open: StateSnackBar;
+    setOpen: Dispatch<SetStateAction<StateSnackBar>>;
+}
+
+export interface StateSnackBar {
+    error: boolean;
+    success: boolean;
+}
+
+function CustomSnackBar({ message, open, setOpen }: SnackBarProps) {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (open) {
+            const timer = setTimeout(() => {
+                setOpen({ error: false, success: false });
+                dispatch(setError(''))
+            }, 6000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [open]);
+
+    const handleClose = () => {
+        setOpen({ error: false, success: false });
+    };
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }}
+            open={open.error || open.success}
+            autoHideDuration={6000}
+            message={message}
+        >
+            <Alert
+                severity={open.error ? "error" : "success"}
+                sx={{ width: '100%' }}
+                action={
+                    <IconButton
+                        size="small"
+                        aria-label="close"
+                        color="inherit"
+                        onClick={handleClose}
+                    >
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                }
+                onClose={handleClose}
+            >
+                {message}
+            </Alert>
+        </Snackbar>
+    );
+}
+
+export default CustomSnackBar;

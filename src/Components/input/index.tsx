0b5739c948e410abd368747ba705fd2a56bb9738
Carlos Eduardo <carlos.bageston@bubo.tech>
2024-11-06 18:58:47 -0300

    refactor input

--- src/Components/input/index.tsx
+++ src/Components/input/index.tsx
@@ -1,65 +1,31 @@
 import React from 'react';
-import { TextField } from '@mui/material';
+import { TextField, TextFieldProps } from '@mui/material';
 
-interface InputProps {
-    key?: React.Key | null | undefined;
-    name: string;
+interface InputProps extends Omit<TextFieldProps, 'error' | 'helperText'> {
     label: string;
-    value: string | number | readonly string[] | undefined | Date;
-    maxLength?: number;
     error?: string;
-    style?: React.CSSProperties;
-    styleLabel?: React.CSSProperties;
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    disabled?: boolean | undefined;
-    onFocus?: boolean | undefined;
-    inputRef?: ((instance: HTMLInputElement | null) => void) | React.RefObject<HTMLInputElement> | null | undefined;
-    type?: React.HTMLInputTypeAttribute | undefined;
-    onKeyUp?: React.KeyboardEventHandler<HTMLInputElement>;
-    onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
-    onKeyPress?: React.KeyboardEventHandler<HTMLInputElement>;
+    heightDiv?: number | string;
+    maxLength?: number;
 }
 
 const Input: React.FC<InputProps> = ({
     label,
-    value,
-    onChange,
-    name,
     error,
-    onBlur,
-    style,
+    heightDiv,
     maxLength,
-    disabled,
-    inputRef,
-    onKeyUp,
-    onKeyDown,
-    onKeyPress,
-    type,
-    key
+    ...rest
 }) => {
     return (
-        <div style={{ height: '70px' }}>
+        <div style={{ height: heightDiv || '70px' }}>
             <TextField
-                key={key}
                 label={label}
-                value={value instanceof Date ? value.toLocaleDateString() : value}
-                onChange={onChange}
-                onBlur={onBlur}
-                name={name}
-                type={type || 'text'}
                 error={!!error}
                 helperText={error}
-                inputProps={{ maxLength }}
-                InputLabelProps={{ shrink: Boolean(value) }}
-                disabled={disabled}
-                inputRef={inputRef}
-                onKeyUp={onKeyUp}
-                onKeyDown={onKeyDown}
-                onKeyPress={onKeyPress}
+                InputLabelProps={{ shrink: Boolean(rest.value) }}
                 variant="standard"
                 fullWidth
-                sx={style}
+                inputProps={{ maxLength }}
+                {...rest}
             />
         </div>
     );
